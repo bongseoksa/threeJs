7658a30a49a77b672669082dd95dd05d539e1828
Todd_사봉석 <bssa@maxst.com>
2024-07-29 14:32:29 +0900

    04 * mesh 오브젝트 position set 설정

--- 04/script.js
+++ 04/script.js
@@ -10,9 +10,10 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const meterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, meterial);
-mesh.position.x = 1;
-mesh.position.y = -0.6;
-mesh.position.z = -1;
+// mesh.position.x = 1;
+// mesh.position.y = -0.6;
+// mesh.position.z = -1;
+mesh.position.set(1, -0.6, -1);
 scene.add(mesh);
 
 // Sizes
@@ -22,7 +23,7 @@ const sizes = { width: 800, height: 600 };
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
-mesh.position.normalize();
+mesh.position.normalize(); // 오브젝트를 씬 중심에서 1 거리에 위치하게 함
 console.log(mesh.position.length());
 console.log(mesh.position.distanceTo(camera.position));
 
