151645f01ef7f2c7201eb639c9558e8323a2fddf
Todd_사봉석 <bssa@maxst.com>
2024-07-31 16:04:49 +0900

    [06] Cameras * Perspected Camera : 원근감을 가지는 카메라

---
+++ 06/README.md
@@ -0,0 +1,9 @@
+## Cameras
+
+### Perspective Camera
+
+- 원근감을 가지는 카메라
+
+### Orthographic Camera
+
+- 원근감이 없는 카메라
---
+++ 06/script.js
@@ -0,0 +1,55 @@
+import * as THREE from 'three';
+
+/** Canvas */
+const canvas = document.querySelector('canvas.webgl');
+
+/** Scene */
+const scene = new THREE.Scene();
+
+/** Object */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+/** Sizes */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/** Camera */
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  1000,
+);
+camera.position.x = 2;
+camera.position.y = 2;
+camera.position.z = 2;
+console.log(camera.position.length());
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+/** Renderer */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+/** Clock */
+const clock = new THREE.Clock();
+
+/** Animation */
+const tick = () => {
+  // Clock
+  const elapsedTime = clock.getElapsedTime();
+
+  mesh.rotation.y = elapsedTime * Math.PI * 0.5;
+
+  // Render
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
